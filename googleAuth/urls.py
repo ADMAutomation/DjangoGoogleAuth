cc4a01906c35791c936d91cc5cd35fd7ebad994b
linuxlover <linuxlover@gmail.com>
2017-04-05 17:53:19 +0200

    Debugging... v 0.0.10

--- googleAuth/urls.py
+++ googleAuth/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from .views import GoToGoogleLoginPage, AfterGoogleLoginPage
+from .urlsAPI import urlpatterns as google_api_urlpatterns
+
 urlpatterns = [
     url(r'^stepOne/$', GoToGoogleLoginPage.as_view(), name="google-auth-step-one"),
     url(r'^stepTwo/$', AfterGoogleLoginPage.as_view(), name="google-auth-step-two"),
